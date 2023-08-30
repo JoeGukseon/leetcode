39998c5d0361217cf81ffcb19fbacb1a988a41e5
JoeGukseon <116003804+JoeGukseon@users.noreply.github.com>
2023-08-30 16:03:04 +0900

    Time: 14 ms (31.53%), Space: 44 MB (9.95%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
         Map<Character, Integer> map = new HashMap<>();
 
         for (char c : t.toCharArray()) {
