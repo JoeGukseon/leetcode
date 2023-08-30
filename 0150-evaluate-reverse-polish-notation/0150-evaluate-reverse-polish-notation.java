12c254de81cf46d8c6e20013bb809bddc4f00042
JoeGukseon <116003804+JoeGukseon@users.noreply.github.com>
2023-08-30 19:15:49 +0900

    Time: 7 ms (58.32%), Space: 43 MB (80.73%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,30 +1,29 @@
 class Solution {
-   public int evalRPN(String[] tokens) {
+    public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
-        int val1,val2;
-        for (String token : tokens) {
-            switch (token) {
-                case "+":
-                    stack.push(stack.pop() + stack.pop());
-                    break;
-                case "-":
-                    val1 = stack.pop();
-                    val2 = stack.pop();
-                    stack.push(val2 - val1);
-                    break;
-                case "*":
-                    stack.push(stack.pop() * stack.pop());
-                    break;
-                case "/":
-                    val1 = stack.pop();
-                    val2 = stack.pop();
-                    stack.push(val2 / val1);
-                    break;
-                default:
-                    stack.push(Integer.parseInt(token));
-                    break;
+        Set<String> ops = Set.of("+", "-", "*", "/");
+
+        for (String s : tokens) {
+            if (ops.contains(s)) {
+                int a = stack.pop();
+                int b = stack.pop();
+                int c = 0;
+                
+                switch(s) {
+                    case "+" -> {c = b + a;}
+                    case "-" -> {c = b - a;}
+                    case "*" -> {c = b * a;}
+                    case "/" -> {c = b / a;}
+                }
+                
+                stack.push(c);
             }
+            
+            else stack.push(Integer.parseInt(s));
         }
-        return stack.pop();
+
+        return stack.peek();
     }
 }
+
+// TC: O(n), SC: O(n)
