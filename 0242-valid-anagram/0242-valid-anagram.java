70b9c94557a76175e390299385c7780252ca4329
JoeGukseon <116003804+JoeGukseon@users.noreply.github.com>
2023-08-30 15:59:33 +0900

    Time: 15 ms (28.81%), Space: 43.9 MB (23.1%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                int cnt = map.get(c);
+                cnt--;
+                if (cnt == 0) {
+                    map.remove(c);
+                } else {
+                    map.put(c, cnt);
+                }
+            } else {
+                return false; //s가 t에 없을때
+            }
+        }
+        return map.isEmpty();
+    }
+}
