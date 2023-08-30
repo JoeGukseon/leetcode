a7c1ce642f283c98cbb4f74048a3e88d358541bd
JoeGukseon <116003804+JoeGukseon@users.noreply.github.com>
2023-08-31 01:31:25 +0900

    Time: 16 ms (39.5%), Space: 43.4 MB (92.82%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,22 +1,17 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> map = new HashMap<>();
-        for (char c : magazine.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-        for (char c : ransomNote.toCharArray()) {
-            if (map.containsKey(c)) {
-                int cnt = map.get(c);
-                cnt--;
-                if (cnt == 0) {
-                    map.remove(c);
-                } else {
-                    map.put(c, cnt);
-                }
-            } else {
-                return false;
-            }
-        }
-        return true;
+        HashMap<Character,Integer> map=new HashMap<>();
+       for(int i=0;i<magazine.length();i++){
+         map.put(magazine.charAt(i),map.getOrDefault(magazine.charAt(i),0)+1);
+       }
+       for(int i=0;i<ransomNote.length();i++){
+           if(map.containsKey(ransomNote.charAt(i)) && map.get(ransomNote.charAt(i))>0){
+               map.put(ransomNote.charAt(i),map.get(ransomNote.charAt(i))-1);
+           }
+           else {
+               return false;
+           }
+       }
+       return true;
     }
 }
