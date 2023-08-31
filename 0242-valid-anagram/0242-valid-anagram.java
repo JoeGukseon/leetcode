40e01c0b80bfa276d0ec908604da79f75f72a7cd
JoeGukseon <116003804+JoeGukseon@users.noreply.github.com>
2023-08-31 12:18:38 +0900

    Time: 3 ms (91.59%), Space: 43.6 MB (41.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,27 +1,11 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-        Map<Character, Integer> map = new HashMap<>();
-
-        for (char c : t.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-
-        for (char c : s.toCharArray()) {
-            if (map.containsKey(c)) {
-                int cnt = map.get(c);
-                cnt--;
-                if (cnt == 0) {
-                    map.remove(c);
-                } else {
-                    map.put(c, cnt);
-                }
-            } else {
-                return false; //s가 t에 없을때
-            }
-        }
-        return map.isEmpty();
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        
+        return Arrays.equals(sChars, tChars);
     }
 }
