35bfae3666ede616273a3cb9dd42fba0ca3ef641
JoeGukseon <116003804+JoeGukseon@users.noreply.github.com>
2023-08-31 20:33:56 +0900

    Time: 13 ms (99.26%), Space: 56.4 MB (79.96%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -6,12 +6,12 @@ class Solution {
             return 1;
         }
         Arrays.sort(nums);
-        int cnt = 1, max = Integer.MIN_VALUE;
+        int cnt = 1, max = 1;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i - 1] == nums[i] - 1) {
                 cnt++;
             } else if (nums[i - 1] == nums[i]) {
-                
+                continue;
             } else {
                 cnt = 1;
             }
