3e7254d522c0ba88d5161f7dcf0d0f37ca567956
JoeGukseon <116003804+JoeGukseon@users.noreply.github.com>
2023-09-07 00:32:36 +0900

    Time: 0 ms (100%), Space: 41 MB (75.15%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,48 +1,20 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+    int maxlevel = 0;
     public List<Integer> rightSideView(TreeNode root) {
-        if (root == null) {
-            return new ArrayList<>();
-        } else if (root.left == null && root.right == null) {
-            return List.of(root.val);
+        List<Integer> list  = new ArrayList<>();
+        right(root,1,list);
+        return list ;
+    }
+    void right(TreeNode root,int level,List<Integer> list){
+        if(root==null){
+            return ;
         }
-        List<Integer> answer = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
-
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            int lvSize = queue.size();
-
-            for (int i = 0; i < lvSize; i++) {
-                TreeNode tempNode = queue.poll();
-                
-                if (i == lvSize-1) {
-                    answer.add(tempNode.val);
-                }
-                if (tempNode.left != null) {
-                    queue.offer(tempNode.left);
-                }
-                if (tempNode.right != null) {
-                    queue.offer(tempNode.right);
-                }
-            }
-
+        if(maxlevel<level){
+            list.add(root.val);
+            maxlevel=level;
         }
-
-        return answer;
+        right(root.right,level+1,list);
+        right(root.left,level+1,list);
+        
     }
 }
