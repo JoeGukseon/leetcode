859f0dc5b5ce613a07fe9d8254c9e04aaf93534b
JoeGukseon <116003804+JoeGukseon@users.noreply.github.com>
2023-10-05 13:12:54 +0900

    Time: 0 ms (100%), Space: 43 MB (67.82%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,26 +15,48 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        if (head == null) {
-            return null;
-        }
+  Node iter = head, next;
 
-        Map<Node, Node> nodeMap = new HashMap<>();
+  // First round: make copy of each node,
+  // and link them together side-by-side in a single list.
+  while (iter != null) {
+    next = iter.next;
 
-        Node current = head;
-        while (current != null) {
-            nodeMap.put(current, new Node(current.val));
-            current = current.next;
-        }
+    Node copy = new Node(iter.val);
+    iter.next = copy;
+    copy.next = next;
 
-        current = head;
-        while (current != null) {
-            Node copy = nodeMap.get(current);
-            copy.next = nodeMap.get(current.next);
-            copy.random = nodeMap.get(current.random);
-            current = current.next;
-        }
+    iter = next;
+  }
 
-        return nodeMap.get(head);
+  // Second round: assign random pointers for the copy nodes.
+  iter = head;
+  while (iter != null) {
+    if (iter.random != null) {
+      iter.next.random = iter.random.next;
     }
+    iter = iter.next.next;
+  }
+
+  // Third round: restore the original list, and extract the copy list.
+  iter = head;
+  Node pseudoHead = new Node(0);
+  Node copy, copyIter = pseudoHead;
+
+  while (iter != null) {
+    next = iter.next.next;
+
+    // extract the copy
+    copy = iter.next;
+    copyIter.next = copy;
+    copyIter = copy;
+
+    // restore the original list
+    iter.next = next;
+
+    iter = next;
+  }
+
+  return pseudoHead.next;
+}
 }
