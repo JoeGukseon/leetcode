a61443f2fbb2e17614ba550f00252ba5bfd2c0bd
JoeGukseon <116003804+JoeGukseon@users.noreply.github.com>
2023-11-04 18:53:26 +0900

    Time: 1 ms (99.6%), Space: 43.4 MB (98.01%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,56 @@
+class Solution {
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return;
+        }
+
+        int rows = board.length;
+        int cols = board[0].length;
+
+        //가장 자리 찾기 행 검색
+        for (int i = 0; i < rows; i++) {
+            if (board[i][0] == 'O') {
+                dfs(board, i, 0);
+            }
+            if (board[i][cols - 1] == 'O') {
+                dfs(board, i, cols - 1);
+            }
+        }
+        // 열 검색
+        for (int j = 0; j < cols; j++) {
+            if (board[0][j] == 'O') {
+                dfs(board, 0, j);
+            }
+            if (board[rows - 1][j] == 'O') {
+                dfs(board, rows - 1, j);
+            }
+        }
+
+        //dfs 탐색 후 변경작업
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == 'E') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int row, int col) {
+        int rows = board.length;
+        int cols = board[0].length;
+
+        if (row < 0 || row >= rows || col < 0 || col >= cols || board[row][col] != 'O') {
+            return;
+        }
+
+        board[row][col] = 'E';
+
+        dfs(board, row - 1, col);
+        dfs(board, row + 1, col);
+        dfs(board, row, col - 1);
+        dfs(board, row, col + 1);
+    }
+}
