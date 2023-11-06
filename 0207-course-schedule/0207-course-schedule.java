94d0b0e2938465dda0ef7003e48af44ff025b4a4
JoeGukseon <116003804+JoeGukseon@users.noreply.github.com>
2023-11-06 16:41:19 +0900

    Time: 5 ms (66.28%), Space: 43.6 MB (86.25%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,50 +1,55 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         // 그래프 생성
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        int[] inDegree = new int[numCourses]; //진입차수로 선수과목의 수
+        HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < numCourses; i++) {
+            graph.put(i, new ArrayList<>());
+        }
 
         // 그래프 초기화
-        for (int i = 0; i < numCourses; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        // 각 과목의 선수과목과 in-degree 설정
         for (int[] prerequisite : prerequisites) {
             int course = prerequisite[0];
-            int preCourse = prerequisite[1];
-            graph.get(preCourse).add(course);
-            inDegree[course]++;
+            int prerequisiteCourse = prerequisite[1];
+            graph.get(prerequisiteCourse).add(course);
         }
 
-        Queue<Integer> queue = new LinkedList<>();
-        // in-degree가 0인 과목을 큐에 추가
+        // 방문 여부를 나타내는 집합
+        HashSet<Integer> visited = new HashSet<>();
+
+        // 각 노드에 대해 DFS 수행
         for (int i = 0; i < numCourses; i++) {
-            if (inDegree[i] == 0) {
-                queue.add(i);
+            if (!dfs(i, graph, visited, new HashSet<>())) {
+                return false; // 순환이 발견되면 false 반환
             }
         }
 
-        // 위상 정렬 수행
-        while (!queue.isEmpty()) {
-            int currentCourse = queue.poll();
-            // 현재 과목의 선수과목들의 in-degree 감소
-            for (int nextCourse : graph.get(currentCourse)) {
-                inDegree[nextCourse]--;
-                // in-degree가 0이 되면 큐에 추가
-                if (inDegree[nextCourse] == 0) {
-                    queue.add(nextCourse);
-                }
-            }
+        return true; // 순환이 없으면 true 반환
+    }
+    private boolean dfs(int node, HashMap<Integer, ArrayList<Integer>> graph, HashSet<Integer> visited, HashSet<Integer> recursionStack) {
+        // 현재 노드를 방문 중인 경우 순환이 발견된 것이므로 false 반환
+        if (recursionStack.contains(node)) {
+            return false;
         }
 
-        // 모든 과목의 in-degree가 0인지 확인
-        for (int degree : inDegree) {
-            if (degree > 0) {
+        // 이미 방문한 노드인 경우 중복 방문을 방지하기 위해 true 반환
+        if (visited.contains(node)) {
+            return true;
+        }
+
+        // 현재 노드를 방문 중임을 표시
+        visited.add(node);
+        recursionStack.add(node);
+
+        // 현재 노드와 연결된 노드에 대해 DFS 수행
+        for (int neighbor : graph.get(node)) {
+            if (!dfs(neighbor, graph, visited, recursionStack)) {
                 return false;
             }
         }
 
-        return true; // 모든 과목을 수강할 수 있는 경우
+        // 현재 노드의 탐색이 완료되면 recursionStack에서 제거
+        recursionStack.remove(node);
+
+        return true;
     }
 }
