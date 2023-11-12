36329f75cd96fa8e084a49d784a7fc329cc71ff9
JoeGukseon <116003804+JoeGukseon@users.noreply.github.com>
2023-11-12 14:16:32 +0900

    Time: 2 ms (61.89%), Space: 6.7 MB (30.78%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.c
@@ -0,0 +1,30 @@
+int search(int* nums, int numsSize, int target) {
+    int left = 0;
+    int right = numsSize - 1;
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        if (nums[left] <= nums[mid]) {
+            // 왼쪽 절반이 정렬되어 있는 경우
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            // 오른쪽 절반이 정렬되어 있는 경우
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
