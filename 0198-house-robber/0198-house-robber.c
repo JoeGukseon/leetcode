ee20f73bdc066e36d9d1704e749bd75e07314511
JoeGukseon <116003804+JoeGukseon@users.noreply.github.com>
2023-11-13 10:00:31 +0900

    Time: 0 ms (100%), Space: 6.7 MB (17.02%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.c
@@ -0,0 +1,19 @@
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int rob(int* nums, int numsSize) {
+    if(numsSize==1)
+        return nums[0];
+
+    int dp[numsSize];
+
+    dp[0] = nums[0];
+    dp[1] = max(nums[0],nums[1]);
+
+    for(int i=2;i<numsSize;i++){
+        dp[i] = max(dp[i-1],dp[i-2]+nums[i]);
+    }
+
+    return dp[numsSize-1];
+}
