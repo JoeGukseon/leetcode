08d97d744e90cc2a9155962da17f301df0fd7471
JoeGukseon <116003804+JoeGukseon@users.noreply.github.com>
2023-11-15 11:27:19 +0900

    Time: 656 ms (89.49%) | Memory: 13.9 MB (44.44%) - LeetSync

---
+++ 322-coin-change/coin-change.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp = [float('inf')] * (amount + 1) # 무한대의 값으로 초기화
+
+        dp[0] = 0
+
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] = min(dp[i], dp[i - coin] + 1) #동전갯수 갱신
+
+        return -1 if dp[amount] == float('inf') else dp[amount]        
