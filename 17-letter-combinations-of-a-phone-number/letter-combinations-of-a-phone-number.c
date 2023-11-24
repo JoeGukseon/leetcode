40b9107bd70e90e4a7d6b276ce38ffc6cf1b4ae6
JoeGukseon <116003804+JoeGukseon@users.noreply.github.com>
2023-11-25 00:24:04 +0900

    Time: 0 ms (100.00%) | Memory: 7.3 MB (9.22%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.c
@@ -0,0 +1,37 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+const char *digitMapping[] = {
+    "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+};
+
+void generateCombinations(char *current, char *digits, int index, char ***result, int *resultSize) {
+    if (index == strlen(digits)) {
+        *result = realloc(*result, (*resultSize + 1) * sizeof(char *));
+        (*result)[*resultSize] = strdup(current);
+        (*resultSize)++;
+        return;
+    }
+
+    int digit = digits[index] - '0';
+    for (int i = 0; i < strlen(digitMapping[digit]); i++) {
+        current[index] = digitMapping[digit][i];
+        current[index + 1] = '\0';
+        generateCombinations(current, digits, index + 1, result, resultSize);
+    }
+}
+
+char** letterCombinations(char* digits, int* returnSize) {
+    char **result = NULL;  // 초기화
+    *returnSize = 0;
+
+    if (digits == NULL || strlen(digits) == 0) {
+        return result;
+    }
+
+    char current[10];  // 최대 조합 길이로 조정
+    generateCombinations(current, digits, 0, &result, returnSize);
+
+    return result;
+}
